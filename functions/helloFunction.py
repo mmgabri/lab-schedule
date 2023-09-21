a490b7b4dab321e285f08cabe111911c96d41da3
mmgabri <marcelomgabriel@gmail.com>
2023-09-21 11:45:08 -0300

    add sns

--- functions/helloFunction.py
+++ functions/helloFunction.py
@@ -1,9 +1,24 @@
 import json
+import boto3
+
+client_sns = boto3.client('sns')
+arn_topic='arn:aws:sns:us-east-1:146570171569:test-email'
+message = 'Corpo da mensagem.....bla bla bla'
+
+def send_email():
+    response = client_sns.publish(
+    TargetArn=arn_topic,
+    Message=json.dumps({'default': json.dumps(message)}),
+    MessageStructure='json'
+)
+    print(f'Response: {response}')
 
 def lambda_handler(event, context):
     print('Lambda Hello trigado!')
 
     print (f'Evento: {event}')
+
+    send_email()
 
     return {
         
